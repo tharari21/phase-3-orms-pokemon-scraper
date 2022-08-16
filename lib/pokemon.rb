6b2318b5de6238456fcadb11047c44f14a0172d2
Tomer Harari <tharari21@gmail.com>
2022-08-16 13:53:55 -0400

    commit tree a0208565641c5a56b1f9515bfcc52f79de6498cb

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,18 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+    def initialize(id:, name:, type:, db: )
+        @id, @name, @type, @db = id,name,type,db
+    end
+    def self.save(name, type, db)
+        sql =<<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+
+    end
+    def self.find(id, db)
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
+        Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+    end
 end
